567493ba33e6570fdeae0af60d1f5e09a1f2190c
Oleg Dantsaranov <oleg.dants@icloud.com>
2019-02-28 18:43:01 +0300

    Add verbose and naive money tranfer implementation

--- src/main/scala/org/odanc/moneytransfer/api/TransactionApi.scala
+++ src/main/scala/org/odanc/moneytransfer/api/TransactionApi.scala
@@ -1,6 +1,7 @@
 package org.odanc.moneytransfer.api
 
 import cats.effect.Effect
+import cats.implicits._
 import io.chrisdavenport.fuuid.circe._
 import io.circe.generic.auto._
 import io.circe.syntax._
@@ -18,7 +19,24 @@ class TransactionApi[F[_]] private(private val repository: AccountRepository[F])
 
     case request @ POST -> Root / TRANSACTIONS =>
       request.decode[Transaction] { transaction =>
-        Created(transaction.asJson)
+        val amount = transaction.amount
+        if (amount <= 0) BadRequest("amount can't have non-positive value".asJson)
+        else if (transaction.from === transaction.to) BadRequest("amount can't be transfered to the same account".asJson)
+        else repository.getAccount(transaction.from) flatMap {
+          case None => NotFound(s"account with id ${transaction.from} doesn't exist".asJson)
+          case Some(from) => repository.getAccount(transaction.to) flatMap {
+            case None => NotFound(s"account with id ${transaction.to} doesn't exist".asJson)
+            case Some(to) =>
+              if (from.amount < amount) BadRequest(s"account ${transaction.from} doesn't have enough amount".asJson)
+              else {
+                val fromAmount = from.amount - amount
+                val toAmount = to.amount + amount
+                repository.updateAccounts(from.copy(amount = fromAmount), to.copy(amount = toAmount)) flatMap { _ =>
+                  Ok(transaction.asJson)
+                }
+              }
+          }
+        }
       }
   }
 }
