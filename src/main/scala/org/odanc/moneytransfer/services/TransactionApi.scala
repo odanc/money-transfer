d82ee8631d9d88ce60c8131f1a3484cf3d0244d7
Oleg Dantsaranov <oleg.dants@icloud.com>
2019-02-28 15:44:38 +0300

    Move transaction endpoint to separate service

---
+++ src/main/scala/org/odanc/moneytransfer/services/TransactionApi.scala
@@ -0,0 +1,30 @@
+package org.odanc.moneytransfer.services
+
+import cats.effect.Effect
+import io.chrisdavenport.fuuid.circe._
+import io.circe.generic.auto._
+import io.circe.syntax._
+import org.http4s.HttpService
+import org.http4s.circe.CirceEntityDecoder._
+import org.http4s.circe.CirceEntityEncoder._
+import org.http4s.dsl.Http4sDsl
+import org.odanc.moneytransfer.models.Transaction
+import org.odanc.moneytransfer.repository.AccountRepository
+
+class TransactionApi[F[_]] private(private val repository: AccountRepository[F])(implicit E: Effect[F]) extends Http4sDsl[F] {
+  private val TRANSACTIONS = "transactions"
+
+  private val service: HttpService[F] = HttpService[F] {
+
+    case request @ POST -> Root / TRANSACTIONS =>
+      request.decode[Transaction] { transaction =>
+        Created(transaction.asJson)
+      }
+  }
+}
+
+object TransactionApi {
+
+  def apply[F[_]](repository: AccountRepository[F]) (implicit E: Effect[F]): HttpService[F] =
+    new TransactionApi[F](repository).service
+}
